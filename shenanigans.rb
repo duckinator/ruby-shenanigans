c5edb3d7c7dc8aa196bceda2ec4ea20a849993d6
Ellen Marie Dash <me@duckie.co>
2017-12-02 01:24:31 -0500

    FUCKING WHAT

--- shenanigans.rb
+++ shenanigans.rb
@@ -2,32 +2,24 @@
 
 def fuck(name)
   method_ = method(name)
-  file, line = method_.source_location
-  def_line = File.readlines(file)[line - 1].strip
-  args = def_line.split(name.to_s, 2).last
-  args = args[1..-2] if args.start_with?('(') && args.end_with?(')')
-  receiver_ = method_.receiver
+  arg_names = method_.parameters.map(&:last).map(&:to_s)
 
+  receiver_ = method_.receiver
   receiver_ = receiver_.class unless receiver_.is_a?(Module)
-  code = <<~EOF
-    alias_method "__fuck_#{name}", #{name.inspect}
-    define_method(#{name.inspect}) do |#{args}|
+
+  receiver_.class_eval do
+    define_method(name) do |*args|
+      args.each_with_index { |value, idx|
+        binding.local_variable_set(arg_names[idx], value)
+      }
       define_method(:arguments) do
-        arg_names  = method(#{name.inspect}).parameters.map(&:last).map(&:to_s)
-        arg_values = arg_names.map(&method(:eval))
+        arg_values = args
         arg_names.zip(arg_values).to_h
       end
 
-      method(:__fuck_#{name}).unbind.bind(binding).call(#{args})
+      method_.call(*args)
     end
-  EOF
-  receiver_.class_eval(code)
-
-  #  method_.send(:binding).define_singleton_method(:arguments) do
-  #    arg_names  = method(name).parameters.map(&:last).map(&:to_s)
-  #    arg_values = arg_names.map(&method(:eval))
-  #    arg_names.zip(arg_values).to_h
-  #  end
+  end
 end
 
 fuck def foo(a, b, c, d)
