3ac46497f7f665d8af0feeac8dc9ca1cd04440b5
Ellen Marie Dash <me@duckie.co>
2017-12-02 02:26:13 -0500

    !!!!!!!!

--- shenanigans.rb
+++ shenanigans.rb
@@ -1,22 +1,22 @@
 #!/usr/bin/env ruby
 
-def fuck(name)
-  method_ = method(name)
-  arg_names = method_.parameters.map(&:last).map(&:to_s)
+module Kernel
+  def fuck(name)
+    return self.class.fuck(name) unless self.is_a?(Module)
 
-  receiver_ = method_.receiver
-  receiver_ = receiver_.class unless receiver_.is_a?(Module)
+    method_ = method(name) rescue instance_method(name)
+    method_ = method_.unbind if method_.respond_to?(:unbind)
+    arg_names = method_.parameters.map(&:last).map(&:to_s)
 
-  receiver_.module_exec do
     define_method(name) do |*arg_values|
       arg_values.each_with_index { |value, idx|
         binding.local_variable_set(arg_names[idx], value)
       }
-      define_method(:arguments) do
+      define_singleton_method(:arguments) do
         arg_names.zip(arg_values).to_h
       end
 
-      method_.call(*arg_values)
+      method_.bind(self).call(*arg_values)
     end
   end
 end
@@ -25,4 +25,11 @@ fuck def foo(a, b, c, d)
   arguments
 end
 
+class Foo
+  fuck def bar(a, b, c, d)
+    arguments
+  end
+end
+
 p foo(1, 2, 3, 4)
+p Foo.new.bar(1, 2, 3, 4)
