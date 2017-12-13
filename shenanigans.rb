d69bdce3df9d4005118d967eb17b7952a14dd226
Ellen Marie Dash <me@duckie.co>
2017-12-13 17:05:26 -0500

    add both validate() and validate!() variants.

--- shenanigans.rb
+++ shenanigans.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
 
+FailedAssertionError = Class.new(ArgumentError)
+
 class Any
   def self.===(arg)
     true
@@ -11,7 +13,7 @@ class AnyOf < Array
   end
 
   def ===(arg)
-    include?(arg)
+    self.any? { |x| validate(x, [arg]) }
   end
 end
 module Kernel
@@ -35,19 +37,25 @@ module Kernel
     end
   end
 
-  def validate(*assertions, arguments)
+  def validate!(*assertions, arguments)
     assertions.zip(arguments).each do |assertion, (_, argument)|
       unless assertion === argument
-        raise ArgumentError, "expected #{assertion}, got #{argument.inspect}"
+        raise FailedAssertionError, "expected #{assertion}, got #{argument.inspect}"
       end
     end
 
     true
   end
+
+  def validate(*assertions, arguments)
+    validate!(*assertions, arguments)
+  rescue FailedAssertionError
+    false
+  end
 end
 
 argy def foo(a, b, c, d)
-  validate(String, Numeric, Any, AnyOf[nil, 4],
+  validate!(String, Numeric, Any, AnyOf[nil, ->(x) { x.even? }],
             arguments)
 end
 
@@ -59,5 +67,6 @@ end
 
 p foo("hi", 2, 3, 4)
 p foo("meep", 2, 3, nil)
+p foo("meep", 2, 3, 6)
 p foo(1, 2, 3, 4)
 #p Foo.new.bar(1, 2, 3, 4)
